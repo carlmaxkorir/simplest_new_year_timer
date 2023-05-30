d949e64269ec591f39c6bcc49510d46fff5ea31d
carlton <carlton@kali.168.0.103>
2023-05-30 11:23:17 +0300

    Initial commit

---
+++ for.py
@@ -0,0 +1,14 @@
+#for index in range(10):
+    #print(index+1)
+
+#for i in range(50, 1000+1,2):
+ #   print(i)
+
+#for i in "Carlton":
+ #   print(i)
+
+import time
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy new year!!")
